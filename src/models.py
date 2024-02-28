fb3e7d6bd37bcb960514ec44adc100cc18ba8285
David Pardo <105815145+DavidPM1990@users.noreply.github.com>
2024-02-28 20:36:24 +0000

    exercise done

--- src/models.py
+++ src/models.py
@@ -15,39 +15,40 @@ class Usuario(Base):
     password = Column(String(250), nullable=False)
     nombre = Column(String(250))
     apellido = Column(String(250))
-    fecha_subscripcion = Column(DateTime)
-    planetas_favoritos = Column(Integer, ForeignKey('planeta_favorito'))
-    personajes_favoritos = Column(Integer, ForeignKey('personaje_favorito'))
+    fecha_subscripcion = Column(DateTime, default=datetime.utcnow)  # Agregado para la fecha de suscripción
+
+    planetas_favoritos = relationship('PlanetaFavorito', foreign_keys='PlanetaFavorito.usuario_id', cascade='all,delete')
+    personajes_favoritos = relationship('PersonajeFavorito', foreign_keys='PersonajeFavorito.usuario_id', cascade='all,delete')
 
 class Planeta(Base):
     __tablename__ = 'planeta'
     id = Column(Integer, primary_key=True)
     nombre = Column(String(250), nullable=False)
-    poblacion = Column(Integer)
-    terreno = Column(Integer)
+    poblacion = Column(Integer)  # Agregado para la población del planeta
 
+    planetas_favoritos = relationship('PlanetaFavorito', foreign_keys='PlanetaFavorito.planeta_id', cascade='all,delete')
 
 class Personaje(Base):
     __tablename__ = 'personaje'
     id = Column(Integer, primary_key=True)
     nombre = Column(String(250), nullable=False)
-    genero = Column(String(50))
-    descripcion = Column(String(250))
-    edad = Column(Integer)
-
+    genero = Column(String(50))  # Agregado para el género del personaje
+    personajes_favoritos = relationship('PersonajeFavorito', foreign_keys='PersonajeFavorito.personaje_id', cascade='all,delete')
 
 class PlanetaFavorito(Base):
     __tablename__ = 'planeta_favorito'
     id = Column(Integer, primary_key=True)
     usuario_id = Column(Integer, ForeignKey('usuario.id'))
     planeta_id = Column(Integer, ForeignKey('planeta.id'))
-
+    usuario = relationship('Usuario', foreign_keys=[usuario_id], cascade='all,delete')
+    planeta = relationship('Planeta', foreign_keys=[planeta_id], cascade='all,delete')
 
 class PersonajeFavorito(Base):
     __tablename__ = 'personaje_favorito'
     id = Column(Integer, primary_key=True)
     usuario_id = Column(Integer, ForeignKey('usuario.id'))
     personaje_id = Column(Integer, ForeignKey('personaje.id'))
-
+    usuario = relationship('Usuario', foreign_keys=[usuario_id], cascade='all,delete')
+    personaje = relationship('Personaje', foreign_keys=[personaje_id], cascade='all,delete')
 
 render_er(Base, 'diagram.png')
