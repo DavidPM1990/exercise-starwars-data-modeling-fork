d83a1afdd7fc9787ff6108ad9f753cb6a1d3c134
David Pardo <105815145+DavidPM1990@users.noreply.github.com>
2024-02-28 20:40:11 +0000

    exercise done

--- src/models.py
+++ src/models.py
@@ -15,8 +15,7 @@ class Usuario(Base):
     password = Column(String(250), nullable=False)
     nombre = Column(String(250))
     apellido = Column(String(250))
-    fecha_subscripcion = Column(DateTime, default=datetime.utcnow)  # Agregado para la fecha de suscripción
-
+    fecha_subscripcion = Column(DateTime, default=datetime.utcnow)
     planetas_favoritos = relationship('PlanetaFavorito', foreign_keys='PlanetaFavorito.usuario_id', cascade='all,delete')
     personajes_favoritos = relationship('PersonajeFavorito', foreign_keys='PersonajeFavorito.usuario_id', cascade='all,delete')
 
@@ -24,15 +23,17 @@ class Planeta(Base):
     __tablename__ = 'planeta'
     id = Column(Integer, primary_key=True)
     nombre = Column(String(250), nullable=False)
-    poblacion = Column(Integer)  # Agregado para la población del planeta
-
+    poblacion = Column(Integer)
+    terreno = Column(Integer)
     planetas_favoritos = relationship('PlanetaFavorito', foreign_keys='PlanetaFavorito.planeta_id', cascade='all,delete')
 
 class Personaje(Base):
     __tablename__ = 'personaje'
     id = Column(Integer, primary_key=True)
-    nombre = Column(String(250), nullable=False)
-    genero = Column(String(50))  # Agregado para el género del personaje
+    nombre = Column(String(150), nullable=False)
+    descripcion = Column(String(250))
+    altura = Column(Integer)
+    genero = Column(String(50))
     personajes_favoritos = relationship('PersonajeFavorito', foreign_keys='PersonajeFavorito.personaje_id', cascade='all,delete')
 
 class PlanetaFavorito(Base):
